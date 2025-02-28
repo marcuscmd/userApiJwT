9072ac8ab77c2420fa47b49d67414fa19eead5ba
Marcus Carvalho <marcus.carvalho@itera360.com.br>
2025-02-27 21:01:39 -0300

    Dominio usuario criado, seguindo arquitetura em camdas para utilização na Api

---
+++ UserApi/Dominio/Entidades/Usuario.cs
@@ -0,0 +1,39 @@
+public class Usuario
+{
+    private string _senha;
+
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string Cpf { get; set; }
+    public string Login { get; set; }
+    public string Email { get; set; }
+    public string Senha { get { return _senha; } }
+    public DateTime DataNascimento { get; set; }
+    public DateTime DataCriacao { get; set; }
+    public bool Ativo { get; set; }
+
+
+
+    public Usuario(string senha)
+    {
+        _senha = senha;
+        DataCriacao = DateTime.Now;
+        Ativo = true;
+    }
+
+    public void AtivarUsuario()
+    {
+        Ativo = true;
+    }
+
+    public void DesativarUsuario()
+    {
+        Ativo = false;
+    }
+
+    public void AlterarSenha(string senha)
+    {
+        _senha = senha;
+    }
+}
